6f72130ffc6a91ef5a497f76b7c66edcaa256462
Ravi <Rpatel1291@gmail.com>
2019-05-06 20:14:26 -0400

    Reset Again

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const cors = require('cors')
+const path = require('path')
+const io = require("socket.io")(http)
+
+const port = 3000
+const userSearch = io.of('/user-search')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname+'/index.html'))
+})
+
+app.listen(port, ()=>{
+    console.log("listening on http://localhost:3000")
+})
